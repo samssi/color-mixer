be711a1ce0d77ff465b9b1a1cbb3f1f3453bdd6a
samssi <samir.hamadi@gmail.com>
2020-02-17 22:07:02 +0200

    Add posting of the request to the led

--- opencv/main.py
+++ opencv/main.py
@@ -1,4 +1,8 @@
 import cv2
+import numpy
+import requests
+
+led_url = 'http://192.168.1.129:5000/color'
 
 
 def start():
@@ -19,13 +23,20 @@ def start():
 
         if frame_nbr == 20:
             frame_nbr = 0
-            print(frame[sample_y, sample_x])
+            b, g, r = frame[sample_y, sample_x]
+
+            try:
+                requests.post(led_url,
+                              json={"r": numpy.int(r),
+                                    "g": numpy.int(g),
+                                    "b": numpy.int(b)}, timeout=0.1)
+            except (ValueError, Exception):
+                print(f'Failed to post rgb [{r} {g} {b}]')
 
         cv2.imshow("result", overlay_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
 
     video.release()
     cv2.destroyAllWindows()
